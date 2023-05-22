1f31e380640e003f4a79793139a4278107e397ea
danladidaniellaemmanuel <daniella.danladi@binghamuni.edu.ng>
2023-05-22 22:07:52 +0100

    Update Energy.java
    private gravity constant value

--- src/PHYSICS/Energy.java
+++ src/PHYSICS/Energy.java
@@ -4,5 +4,5 @@ public class Energy extends Physics {
     public float calculatePotentialEnergy(float mass, float height) {
         return mass * getGravity() * height;
     }
-
+    private float gravity = 10f;
 }
