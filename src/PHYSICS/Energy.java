bcc579dacf21cc27659663ac6316ce1cbd876a95
danladidaniellaemmanuel <daniella.danladi@binghamuni.edu.ng>
2023-05-22 22:51:22 +0100

    Update Energy.java

--- src/PHYSICS/Energy.java
+++ src/PHYSICS/Energy.java
@@ -4,5 +4,18 @@ public class Energy extends Physics {
     public float calculatePotentialEnergy(float mass, float height) {
         return mass * getGravity() * height;
     }
-    private float gravity = 10f;
+    public float calculateKineticEnergy(float mass, float velocity) {
+        return (1/2) * mass * velocity* velocity;
+    }
+
+    public float calculateForce(float mass,float distance) {
+        return mass * distance;
+    }
+
+    public float calculatePower(float work, float time) {
+        return (work/time);
+    }
+    public float calculateWorkDone (float force, float distance1) {
+        return force * distance1;
+    }    
 }
