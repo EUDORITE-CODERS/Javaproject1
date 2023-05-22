7f5341f35f98a8568784aed82a47636ca6d2375f
danladidaniellaemmanuel <daniella.danladi@binghamuni.edu.ng>
2023-05-22 22:45:48 +0100

    Update Main.java
    Update on Kinetic Energy,Power, Force and Workdone

--- src/PHYSICS/Main.java
+++ src/PHYSICS/Main.java
@@ -14,7 +14,11 @@ public class Main {
 
         System.out.println("Please choose an operation:");
         System.out.println("1. Calculate potential energy");
-
+        System.out.println("2. Calculate kinetic energy");
+        System.out.println("3. Calculate Force");
+        System.out.println("4. Calculate power");
+        System.out.println("5. Calculate WorkDone");
+        
         try {
             int choice = input.nextInt();
             float result = 0;
@@ -26,6 +30,37 @@ public class Main {
                     System.out.println("Enter the height (in meters):");
                     float height = input.nextFloat();
                     result = Object.calculatePotentialEnergy(mass, height);
+                    break;
+                case 2:
+                    System.out.println("Enter the mass (in kg):");
+                    float mass1 = input.nextFloat();
+                    System.out.println("Enter the velocity (in m/s):");
+                    float velocity = input.nextFloat();
+                    result = Object.calculateKineticEnergy(mass1, velocity);
+                    break;
+
+                case 3:
+                    System.out.println("Enter the mass (in kg):");
+                    float mass2 = input.nextFloat();
+                    System.out.println("Enter the distance (in meters):");
+                    float distance = input.nextFloat();
+                    result = Object.calculateForce(mass2, distance);
+                    break;
+
+                case 4:
+                    System.out.println("Enter the work (in J):");
+                    float work = input.nextFloat();
+                    System.out.println("Enter the time (in seconds):");
+                    float time = input.nextFloat();
+                    result = Object.calculatePower(work, time);
+                    break;
+
+                case 5:
+                    System.out.println("Enter the force (in Newton):");
+                    float force = input.nextFloat();
+                    System.out.println("Enter the distance (in meters):");
+                    float distance1 = input.nextFloat();
+                    result = Object.calculateWorkDone(force, distance1);
                     break;
 
             }
