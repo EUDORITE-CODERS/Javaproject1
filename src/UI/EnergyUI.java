012acbfa42f1630e308fb28d546d8f3ebff93223
danladidaniellaemmanuel <daniella.danladi@binghamuni.edu.ng>
2023-05-23 11:29:21 +0100

    Update EnergyUI.java
    Completed code

--- src/UI/EnergyUI.java
+++ src/UI/EnergyUI.java
@@ -22,6 +22,10 @@ public class EnergyUI {
         });
 
         frame.add(potentialEnergyBtn);
+        frame.add(kineticEnergyBtn);
+        frame.add(ForceBtn);
+        frame.add(powerBtn);
+        frame.add(WorkDoneBtn);
         frame.setSize(400, 300);
         frame.setLayout(new GridLayout(5, 1));
         frame.setVisible(true);
@@ -63,6 +67,147 @@ public class EnergyUI {
 
         potentialEnergyFrame.setVisible(true);
     }
+
+}
+JFrame kineticEnergyFrame = new JFrame("Kinetic Energy");
+JLabel massLabel3 = new JLabel("Enter mass (kg):");
+JLabel velocityLabel = new JLabel("Enter velocity (m/s):");
+JTextField massTextField3 = new JTextField();
+JTextField velocityTextField = new JTextField();
+JButton calculateKineticEnergyBtn = new JButton("Calculate");
+
+public void drawKineticEnergyUI() {
+    Energy myObj = new Energy();
+    kineticEnergyFrame.setLayout(new GridLayout(3, 2));
+    kineticEnergyFrame.setSize(300, 150);
+    kineticEnergyFrame.add(massLabel3);
+    kineticEnergyFrame.add(massTextField3);
+    kineticEnergyFrame.add(velocityLabel);
+    kineticEnergyFrame.add(velocityTextField);
+    kineticEnergyFrame.add(calculateKineticEnergyBtn);
+
+    calculateKineticEnergyBtn.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try{
+                float mass = Float.parseFloat(massTextField3.getText());
+                float velocity = Float.parseFloat(velocityTextField.getText());
+                float result = myObj.calculateKineticEnergy(mass, velocity);
+                JOptionPane.showMessageDialog(null, "Kinetic Energy = " + result + "kgm/s");
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Invalid input. Please enter valid numbers.");
+            }
+        }
+    });
+
+    kineticEnergyFrame.setVisible(true);
+}
+
+
+JFrame ForceFrame = new JFrame("Force!");
+JLabel mass1Label = new JLabel("Enter mass1 (kg):");
+JLabel distanceLabel = new JLabel("Enter Distance (m):");
+JTextField mass1TextField = new JTextField();
+JTextField distanceTextField = new JTextField();
+JButton calculateForceBtn = new JButton("Calculate");
+
+public void drawForceUI() {
+    Energy myObj = new Energy();
+    ForceFrame.setLayout(new GridLayout(3, 2));
+    ForceFrame.setSize(300, 200);
+    ForceFrame.add(mass1Label);
+    ForceFrame.add(mass1TextField);
+    ForceFrame.add(distanceLabel);
+    ForceFrame.add(distanceTextField);
+    ForceFrame.add(calculateForceBtn);
+
+    calculateForceBtn.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                 float mass1 = Float.parseFloat(mass1TextField.getText());
+                 float distance = Float.parseFloat(distanceTextField.getText());
+                 float result = myObj.calculateForce(mass1, distance);
+                 JOptionPane.showMessageDialog(null, "Force = " + result + "N");
+             } catch (NumberFormatException ex) {
+                 JOptionPane.showMessageDialog(null, "Invalid input. Please enter valid numbers.");
+             }
+        }
+     });
+
+     ForceFrame.setVisible(true);
+}
+JFrame powerFrame = new JFrame("Power");
+JLabel workLabel = new JLabel("Enter work(J):");
+JLabel timeLabel = new JLabel("Enter time(s)");
+JTextField workTextField = new JTextField();
+JTextField timeTextField = new JTextField();
+JButton calculatePowerBtn = new JButton("Calculate");
+
+public void drawPowerUI() {
+    Energy myObj = new Energy();
+    powerFrame.setLayout(new GridLayout(3, 2));
+    powerFrame.setSize(300, 200);
+    
+    powerFrame.add(workLabel);
+    powerFrame.add(workTextField);
+    powerFrame.add(timeLabel);
+    powerFrame.add(timeTextField);
+    powerFrame.add(calculatePowerBtn);
+
+    calculatePowerBtn.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                 float work = Float.parseFloat(workTextField.getText());
+                 float time = Float.parseFloat(timeTextField.getText());
+                 float result = myObj.calculatePower(work, time);
+                 JOptionPane.showMessageDialog(null, "Power = " + result + " W");
+             } catch (NumberFormatException ex) {
+                 JOptionPane.showMessageDialog(null, "Invalid input. Please enter valid numbers.");
+             }
+        }
+    });
+
+    powerFrame.setVisible(true);
+}
+
+JFrame WorkDoneFrame = new JFrame("WorkDone");
+JLabel forceLabel = new JLabel("Enter force:");
+JLabel distance1Label = new JLabel("Enter distance");
+JTextField forceTextField = new JTextField();
+JTextField distance1TextField = new JTextField();
+JButton calculateWorkDoneBtn = new JButton("Calculate");
+
+
+public void drawWorkDoneUI() {
+    Energy myObj = new Energy();
+    WorkDoneFrame.setLayout(new GridLayout(4, 3));
+    WorkDoneFrame.setSize(300, 200);
+
+    WorkDoneFrame.add(forceLabel);
+    WorkDoneFrame.add(forceTextField);
+    WorkDoneFrame.add(distance1Label);
+    WorkDoneFrame.add(distance1TextField);
+    WorkDoneFrame.add(calculateWorkDoneBtn);
+
+    calculateWorkDoneBtn.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                 float force = Float.parseFloat(forceTextField.getText());
+                 float distance1 = Float.parseFloat(distance1TextField.getText());
+                 float result = myObj.calculateWorkDone(force, distance1);
+                 JOptionPane.showMessageDialog(null, "WorkDone = " + result + "J");
+             } catch (NumberFormatException ex) {
+                 JOptionPane.showMessageDialog(null, "Invalid input. Please enter valid numbers.");
+             }
+        }
+    });
+
+    WorkDoneFrame.setVisible(true);
+}
+
 
 }
 
